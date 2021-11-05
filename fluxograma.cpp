bfc66e2931506a1c23f97c5fa0c9d5fb0efad382
Gabriell Tavares <gabrielltavsb@gmail.com>
2021-11-05 17:36:53 -0300

    mudei pra C++

---
+++ fluxograma.cpp
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <sqlite3.h>
+
+#define MAX_CHAR 256
+
+using namespace std;
+
+static int callback(void* data, int argc, char** argv, char** azColName)
+{
+    const char *id = "id";
+    for (int i = 0; i < argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        if (strcmp(azColName[i], id) == 0){
+            cout << "Entrou aqui!" << endl;
+            data = (char *)argv[i];
+            cout << data << endl;
+        }
+    }
+  
+    printf("\n");
+    return 0;
+}
+
+int get_client(sqlite3 *db, string username, string password){
+    string user_id;
+    cout << user_id << endl;
+    string sql_query = "SELECT *\nFROM user\nWHERE name = '" + username + "' \nAND password = '" + password + "';";
+    int rc = sqlite3_exec(db, sql_query.c_str(), callback, &user_id, NULL);
+    
+    return 1;
+}
+
+// Insere novo cliente na tabela user
+int insert_client(sqlite3 *db){
+    char username[MAX_CHAR];
+    char user_password[MAX_CHAR];
+    char *err_msg;
+
+    printf("Digite seu nome de usuário: ");
+    fgets(username, MAX_CHAR, stdin);
+    username[strlen(username)-1] = '\0';
+
+    printf("Digite sua senha: ");
+    fgets(user_password, MAX_CHAR, stdin);
+    user_password[strlen(user_password)-1] = '\0';
+
+    string sql_final;
+    string sql1 = "INSERT INTO user (name, password)\nVALUES('";
+    string sql2 = "', '";
+    string sql3 = "');";
+
+    sql_final = sql1 + username + sql2 + user_password + sql3;
+
+    int rc = sqlite3_exec(db, sql_final.c_str(), NULL, 0, &err_msg);
+    
+    if (rc != SQLITE_OK ) {
+        cerr << "Error Insert" << endl;
+        
+        sqlite3_free(err_msg);        
+        sqlite3_close(db);
+        
+        return -1;
+    }
+    printf("Usuário incluido com sucesso!\n");
+
+    // Deve retornar o id do usuário
+    return 1;
+}
+
+int insere_senha(sqlite3 *db, int user_id){
+    char website[MAX_CHAR];
+    char password[MAX_CHAR];
+    char *err_msg;
+
+    printf("Digite nome do website: ");
+    fgets(website, MAX_CHAR, stdin);
+    website[strlen(website)-1] = '\0';
+
+    printf("Digite sua senha: ");
+    fgets(password, MAX_CHAR, stdin);
+    password[strlen(password)-1] = '\0';
+
+    string sql_final;
+
+    string sql1 = "INSERT INTO password (user_id, website, password)\nVALUES('";
+    string sql2 = "', '";
+    string sql3 = "');";
+
+    sql_final = sql1 + to_string(user_id) + sql2 + website + sql2 + password + sql3;
+
+    int rc = sqlite3_exec(db, sql_final.c_str(), NULL, 0, &err_msg);
+    
+    if (rc != SQLITE_OK ) {
+        cerr << "Insert Password Error!" << endl;
+        
+        sqlite3_free(err_msg);        
+        sqlite3_close(db);
+        
+        return -1;
+    }
+
+    printf("Senha incluida com sucesso!\n");
+    return 1;
+}
+
+int main() {
+    
+    sqlite3 *db;
+    char *err_msg = 0;
+    
+    int rc = sqlite3_open("test.db", &db);
+    
+    if (rc != SQLITE_OK) {
+        
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        return 1;
+    }
+    
+    get_client(db, "Teste 1", "123456");
+    //insert_client(db);
+    //insere_senha(db, 1);
+
+    sqlite3_close(db);
+    
+    return 0;
+}
