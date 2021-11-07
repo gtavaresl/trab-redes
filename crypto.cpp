209fe4e84d56a4080922821e2e2ef79ef43d3313
Gabriell Tavares <gabrielltavsb@gmail.com>
2021-11-07 17:00:44 -0300

    implementação da cifra de cesar

--- crypto.cpp
+++ crypto.cpp
@@ -3,17 +3,26 @@
 
 using namespace std;
 
-string encryptyfunc(string message, int key) {
-    for(int i = 0; i < message.length(); i++)
-        message[i] += key;
-    return message;
+// Implementação da cifra de cesar
+
+string rotate(string msg, int shift){
+    for (int i = 0; i < msg.length(); i++)
+    {
+        if (msg[i] >= 'a' && msg[i] <= 'z')
+            msg[i] = 'a' + (msg[i] - 'a' + shift) % 26;
+        else if (msg[i] >= 'A' && msg[i] <= 'Z')
+            msg[i] = 'A' + (msg[i] - 'A' + shift) % 26;
+    }
+
+    return msg;
 }
 
-string decryptfunc(string message, int key) {
+string encode(string msg, int shift){
+    return rotate(msg, shift);
+}
 
-    for(int i = 0; i < message.length(); i++)
-        message[i] -= key;
-    return message;
+string decode(string msg, int shift){
+    return rotate(msg, 26 - shift);
 }
 
 int main()
@@ -22,11 +31,11 @@ int main()
     string message;
     cout << "Message: " << endl;
     getline(cin, message);
-    int key = rand() % 256;
+    int key = rand() % 94;
     
-    string encrypted = encryptyfunc(message, key);
+    string encrypted = encode(message, key);
     cout << "Encrypted message:" << encrypted <<endl;
-    string decrypted = decryptfunc(encrypted, key);
+    string decrypted = decode(encrypted, key);
     cout << "Decrypted message:" << decrypted << endl;
     
     return 0;
